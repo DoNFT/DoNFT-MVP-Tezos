af58f8402e979c756a5c5b7fea939aa1a5333c2d
matvey20.18 <matvey20.18@bk.ru>
2022-04-19 18:52:32 +0300

    auto deploy update

--- contract/deployment/deploy.ts
+++ contract/deployment/deploy.ts
@@ -2,14 +2,17 @@ import * as dotenv from "dotenv";
 import { TezosToolkit } from "@taquito/taquito";
 import { InMemorySigner } from "@taquito/signer";
 
-dotenv.config(); /* This loads the variables in your .env file to process.env */
+console.log(dotenv, '----------dotenv----------')
 
-console.log(process.env, 'ENV')
+const TEZOS_RPC_URL='https://hangzhounet.smartpy.io/'
+const DEPLOYER_PRIVATE_KEY='edskRxyMAF9gvR21Eiu4qU5sBgWeycuYmwjDcHAZ8dqsSsDRgfTiL9LKNjyRAt6TeKqf1WpmzhXQeYGWQgYsDui8Ar1bgojaGs'
+
 
 const deploy = async () => {
-  const { TEZOS_RPC_URL, ORIGINATOR_PRIVATE_KEY } = process.env;
+  dotenv.config({path: '../.env'}); /* This loads the variables in your .env file to process.env */
+  console.log(TEZOS_RPC_URL, DEPLOYER_PRIVATE_KEY, '----------ENV----------')
 
-  const signer = await InMemorySigner.fromSecretKey(ORIGINATOR_PRIVATE_KEY);
+  const signer = await InMemorySigner.fromSecretKey(DEPLOYER_PRIVATE_KEY);
   const Tezos = new TezosToolkit(TEZOS_RPC_URL);
   Tezos.setProvider({ signer: signer });
 
